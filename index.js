3912ae8f4c808e2fa8e8e640f16586efe1188035
I8302 <198525970+I8302@users.noreply.github.com>
2025-05-19 04:30:10 -0400

    Updated index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { createClient } = require('@supabase/supabase-js');
-
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
